c527239c90805cd5e4edabad09d56516585c608e
crowddevelopers08-arch <crowddevelopers08@gmail.com>
2026-09-18 13:45:24 +0530

    new lpsss smv ayurveda lpssss

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -34,7 +34,7 @@ export default function Footer() {
           title="Contact Info - Mumbai"
           dividerWidth="w-[71%]"
           address="Palkar Polyclinic, Senapati Bapat Square, Ranade Road, Dadar(West)"
-          notes={["Every 2nd Sunday Of The Month", "Calling Time - 10 AM to 9 PM"]}
+          notes={[""]}
           mapTitle="SMVA Mumbai location"
           mapSrc="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3771.7916271273893!2d72.83921367520422!3d19.02890148216606!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7cecf74d3536d%3A0xadc9971f08a548d0!2sPalkars%20Polyclinic!5e0!3m2!1sen!2sin!4v1745299649039!5m2!1sen!2sin"
         />
