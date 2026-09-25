4f3ff6488b2477f92f0f747b2bf367d9e78b9442
crowddevelopers08-arch <crowddevelopers08@gmail.com>
2026-09-25 15:50:42 +0530

    gtags addedx

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,50 +40,89 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${mukta.variable} ${poppins.variable} ${sen.variable} h-full antialiased`}
     >
       <head>
-        {/* Google Ads (gtag.js) */}
+        {/* ============ 1. Google Tag Manager (MUST be first) ============ */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-T3H4LJSH');
+            `,
+          }}
+        />
+
+        {/* ============ 2. Google Ads (gtag.js) ============ */}
         <Script
           id="google-ads"
           strategy="afterInteractive"
           src="https://www.googletagmanager.com/gtag/js?id=AW-18173474730"
         />
-        <Script id="google-ads-init" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-18173474730');
-          `}
-        </Script>
+        <Script
+          id="google-ads-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-18173474730');
+            `,
+          }}
+        />
 
-        {/* Meta Pixels */}
-        <Script id="meta-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '882738111572328');
-            fbq('track', 'PageView');
-          `}
-        </Script>
+        {/* ============ 3. Meta Pixel ============ */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '882738111572328');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
 
-        {/* Microsoft Clarity */}
-        <Script id="microsoft-clarity" strategy="afterInteractive">
-          {`
-            (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "xnszrnoftn");
-          `}
-        </Script>
+        {/* ============ 4. Microsoft Clarity ============ */}
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "xnszrnoftn");
+            `,
+          }}
+        />
       </head>
+
       <body className="min-h-full flex flex-col font-poppins">
-        {/* Meta Pixel Noscript Fallback */}
+        {/* ============ GTM noscript (MUST be first in <body>) ============ */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T3H4LJSH"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="gtm"
+          />
+        </noscript>
+
+        {/* ============ Meta Pixel noscript fallback ============ */}
         <noscript>
           <img
             height="1"
